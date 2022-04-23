161d474320e8b35c473d62c90eb36dd95d3af410
Mason Anders <mason.anders@gopuff.com>
2022-04-23 16:41:06 -0700

    Early return if no window

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,17 @@ import { Session } from 'next-auth';
 import { SessionProvider, signOut } from 'next-auth/react';
 import '../styles/globals.css';
 
-function SafeHydrate({ children }) {
-  return (
-    <div suppressHydrationWarning>
-      {typeof window === 'undefined' ? null : children}
-    </div>
-  );
-}
-
 function Application({ Component, pageProps: { session, ...pageProps } }) {
+  if (typeof window === 'undefined') return null;
+
   if ((session as Session)?.forceSignout) {
     signOut();
   }
 
   return (
-    <SafeHydrate>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </SafeHydrate>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 }
 
