9965f5dd9f5b7e542c40a06742e1d99bbb1a71ce
Mason Anders <mason.anders@gopuff.com>
2022-04-24 01:40:00 -0700

    Hack to remove quary params

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import { Session } from 'next-auth';
 import { SessionProvider, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
 function Application({ Component, pageProps: { session, ...pageProps } }) {
   if (typeof window === 'undefined') return null;
+  const { query, replace, pathname } = useRouter();
+  console.log(query);
+  if (Object.keys(query).length) {
+    console.log(pathname);
+    replace(pathname, undefined, { shallow: true });
+  }
 
   if ((session as Session)?.forceSignout) {
     signOut();
