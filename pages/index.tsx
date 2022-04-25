0cfd0c8226a97cef56f5ec8966e0561c45bc6642
Mason Anders <mason.anders@gopuff.com>
2022-04-24 17:30:37 -0700

    *DO NOT DEPLOY* Log scopes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,24 @@ import { Typography } from '@mui/material';
 import { GetServerSidePropsContext } from 'next';
 import { getServerSession } from 'next-auth';
 import Layout from '../components/Layout';
+import {
+  ADMIN_SCOPE,
+  DIRECTOR_SCOPE,
+  LOGIN_SCOPE,
+  PRIVATE_SCOPE,
+  PUBLIC_SCOPE,
+} from '../util/constants';
 import { validateSession } from '../util/validateSession';
 import { authOptions } from './api/auth/[...nextauth]';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
+  console.log(
+    DIRECTOR_SCOPE,
+    ADMIN_SCOPE,
+    PRIVATE_SCOPE,
+    PUBLIC_SCOPE,
+    LOGIN_SCOPE
+  );
   const session = await getServerSession(context, authOptions);
   const { redirect } = validateSession(session);
   if (redirect) return { redirect };
@@ -14,6 +28,14 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 }
 
 export default function Home() {
+  console.log(
+    DIRECTOR_SCOPE,
+    ADMIN_SCOPE,
+    PRIVATE_SCOPE,
+    PUBLIC_SCOPE,
+    LOGIN_SCOPE
+  );
+
   return (
     <Layout>
       <Typography sx={{ mt: 8 }} textAlign="center" variant="h1">
