a794e0f6c8c692a372770234dda6ba295aa2c356
Mason Anders <mason.anders@gopuff.com>
2022-04-24 17:44:13 -0700

    Revert scope log

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,34 +2,18 @@ import { Typography } from '@mui/material';
 import { GetServerSidePropsContext } from 'next';
 import { getServerSession } from 'next-auth';
 import Layout from '../components/Layout';
-import {
-  ADMIN_SCOPE,
-  DIRECTOR_SCOPE,
-  LOGIN_SCOPE,
-  PRIVATE_SCOPE,
-  PUBLIC_SCOPE,
-} from '../util/constants';
 import { validateSession } from '../util/validateSession';
 import { authOptions } from './api/auth/[...nextauth]';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const scopes = {
-    DIRECTOR_SCOPE: [...DIRECTOR_SCOPE],
-    ADMIN_SCOPE: [...ADMIN_SCOPE],
-    PRIVATE_SCOPE: [...PRIVATE_SCOPE],
-    PUBLIC_SCOPE: [...PUBLIC_SCOPE],
-    LOGIN_SCOPE: [...LOGIN_SCOPE],
-  };
   const session = await getServerSession(context, authOptions);
   const { redirect } = validateSession(session);
   if (redirect) return { redirect };
 
-  return { props: { session, scopes } };
+  return { props: { session } };
 }
 
-export default function Home({ scopes }) {
-  console.log('SCOPES', scopes);
-
+export default function Home() {
   return (
     <Layout>
       <Typography sx={{ mt: 8 }} textAlign="center" variant="h1">
