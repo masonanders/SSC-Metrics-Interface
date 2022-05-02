cd4e6847e8ac47ec4b68a54da0cdf87900c8dc05
Mason Anders <mason.anders@gopuff.com>
2022-05-02 00:07:34 -0700

    Add option to filter orders accepted by others

--- components/AcceptedAndAllOrdersTables.tsx
+++ components/AcceptedAndAllOrdersTables.tsx
@@ -16,13 +16,15 @@ export default function AcceptedAndOpenTables<R extends Request>({
   const rowHead = rows[0];
   const [showAcceptedCompletedOrders, toggleShowAcceptedCompletedOrders] =
     useState<boolean>(false);
+  const [showOpenAcceptedOrders, toggleShowOpenAcceptedOrders] =
+    useState<boolean>(false);
   const [showOpenCompletedOrders, toggleShowOpenCompletedOrders] =
     useState<boolean>(false);
   const { acceptedRows, openRows } = useMemo(
     () =>
       rows.slice(1).reduce<{
         acceptedRows: { incomplete: R[]; complete: R[] };
-        openRows: { incomplete: R[]; complete: R[] };
+        openRows: { open: R[]; accepted: R[]; complete: R[] };
       }>(
         (acc, row) => {
           if (session.data.member.nick === row.acceptedBy) {
@@ -30,14 +32,16 @@ export default function AcceptedAndOpenTables<R extends Request>({
               row
             );
           } else {
-            acc.openRows[row.completed ? 'complete' : 'incomplete'].push(row);
+            acc.openRows[
+              row.completed ? 'complete' : row.acceptedBy ? 'accepted' : 'open'
+            ].push(row);
           }
 
           return acc;
         },
         {
           acceptedRows: { incomplete: [], complete: [] },
-          openRows: { incomplete: [], complete: [] },
+          openRows: { open: [], accepted: [], complete: [] },
         }
       ),
     [session, rows]
@@ -88,11 +92,43 @@ export default function AcceptedAndOpenTables<R extends Request>({
         />
       </Box>
       <Box display="flex" flexDirection="column" overflow="hidden">
-        <Box display="flex" justifyContent="space-between">
-          <Typography fontWeight="bold" variant="h6">
+        <Box
+          display="grid"
+          gridTemplateColumns="[title-start] auto [title-end] auto [accepted-checkbox-start] min-content [accepted-checkbox-end] 24px [completed-checkbox-start] min-content [completed-checkbox-end]"
+        >
+          <Typography fontWeight="bold" variant="h6" gridColumn="title">
             Open orders
           </Typography>
-          <Box display="flex" alignItems="center">
+          <Box
+            display="flex"
+            alignItems="center"
+            gridColumn="accepted-checkbox"
+          >
+            <Checkbox
+              disabled={!openRows.accepted.length}
+              onClick={() =>
+                toggleShowOpenAcceptedOrders(!showOpenAcceptedOrders)
+              }
+              checked={showOpenAcceptedOrders}
+              size="small"
+            />
+            <Typography
+              whiteSpace="nowrap"
+              variant="body2"
+              color={(theme) =>
+                theme.palette.text[
+                  openRows.accepted.length ? 'primary' : 'disabled'
+                ]
+              }
+            >
+              Show accepted
+            </Typography>
+          </Box>
+          <Box
+            display="flex"
+            alignItems="center"
+            gridColumn="completed-checkbox"
+          >
             <Checkbox
               disabled={!openRows.complete.length}
               onClick={() =>
@@ -102,6 +138,7 @@ export default function AcceptedAndOpenTables<R extends Request>({
               size="small"
             />
             <Typography
+              whiteSpace="nowrap"
               variant="body2"
               color={(theme) =>
                 theme.palette.text[
@@ -116,7 +153,8 @@ export default function AcceptedAndOpenTables<R extends Request>({
         <Table
           rows={[
             rowHead,
-            ...openRows.incomplete,
+            ...openRows.open,
+            ...(showOpenAcceptedOrders ? openRows.accepted : []),
             ...(showOpenCompletedOrders ? openRows.complete : []),
           ]}
           RowConstructor={RowConstructor}
