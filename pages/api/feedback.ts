8ad762e8827971d3a840840016d4dca2e9b11fcf
Mason Anders <mason.anders@gopuff.com>
2022-05-02 18:52:25 -0700

    Add feedback api endpoint

---
+++ pages/api/feedback.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import appendSheetData from '../../util/server/googleSheets/appendSheetData';
+import { Sheet } from '../../util/server/googleSheets/types';
+import isSessionValid from '../../util/server/isSessionValid';
+import { authOptions } from './auth/[...nextauth]';
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const session = await getServerSession({ req, res }, authOptions);
+
+  if (isSessionValid(session)) {
+    try {
+      const body: { message: string } = JSON.parse(req.body);
+      const { message } = body;
+
+      if (!message) {
+        res.status(400).json({ message: 'Invalid message' });
+        res.end();
+        return;
+      }
+
+      await appendSheetData({
+        sheet: Sheet.FEEDBACK,
+        values: [
+          [
+            new Date().toUTCString(),
+            session.member.user.username,
+            session.member.nick,
+            message,
+          ],
+        ],
+      });
+      res.status(200).json({ succss: true });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error });
+    }
+  } else {
+    res.status(401).json({ message: 'Endpoint restricted' });
+  }
+}
